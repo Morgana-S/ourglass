529d98954d6de01f11e55021a9cd5bc955aeacf1
Morgana-S <st35giv9@students.codeinstitute.net>
2025-05-26 14:03:03 +0100

    Add event_views for basic site setup

--- ourglass/urls.py
+++ ourglass/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from events import views as events_views
 
 urlpatterns = [
+    path('', events_views.index, name='index'),
     path('admin/', admin.site.urls),
 ]
