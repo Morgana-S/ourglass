a8aba43a8c1ccb1c8277133992b71b15c977cf7d
Morgana-S <st35giv9@students.codeinstitute.net>
2025-05-28 12:55:57 +0100

    Register Summernote-based admin models

--- events/admin.py
+++ events/admin.py
@@ -1,5 +1,21 @@
 from django.contrib import admin
 from .models import Event, Booking
+from django_summernote.admin import SummernoteModelAdmin
+
+
+@admin.register(Event)
+class EventAdmin(SummernoteModelAdmin):
+
+    list_display = ('id', 'event_name', 'event_date',)
+    search_fields = ['event_name']
+    summernote_fields = ('long_description',)
+
+
+@admin.register(Booking)
+class BookingAdmin(SummernoteModelAdmin):
+
+    list_display = ('id', 'event', 'tickets', 'ticketholder',)
+    search_fields = ['event', 'ticketholder']
+
+
 # Register your models here.
-admin.site.register(Event)
-admin.site.register(Booking)
