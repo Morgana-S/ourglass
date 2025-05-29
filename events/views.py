524df83edb80deb637a990701935010faa4fe683
Morgana-S <st35giv9@students.codeinstitute.net>
2025-05-29 15:10:27 +0100

    Update EventList to only show last 5 events

--- events/views.py
+++ events/views.py
@@ -1,10 +1,12 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Event
 # Create your views here.
 
 
-class EventList(generic.ListView):
-    queryset = Event.objects.all()
-    template_name = 'events/event_list.html'
+class LatestEventList(generic.ListView):
+    """
+    Returns a list of all event objects, ordered by the latest created.
+    """
+    queryset = Event.objects.all().order_by('-created_on')[:5]
+    template_name = 'events/index.html'
