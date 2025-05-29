5bb5b18d78e6c5f105ed27b7ed78d5ea25e3ac3c
Morgana-S <st35giv9@students.codeinstitute.net>
2025-05-29 15:14:19 +0100

    Move registered models in admin.py and add more fields for display and filtering

--- events/admin.py
+++ events/admin.py
@@ -2,13 +2,15 @@ from django.contrib import admin
 from .models import Event, Booking
 from django_summernote.admin import SummernoteModelAdmin
 
+# Register your models here.
 
 @admin.register(Event)
 class EventAdmin(SummernoteModelAdmin):
 
-    list_display = ('id', 'event_name', 'event_date',)
+    list_display = ('id', 'event_name', 'event_date', 'event_organiser', 'created_on', 'updated_on',)
     search_fields = ['event_name']
     summernote_fields = ('long_description',)
+    list_filter = ('event_organiser', 'event_date', 'created_on', 'updated_on',)
 
 
 @admin.register(Booking)
@@ -16,6 +18,3 @@ class BookingAdmin(SummernoteModelAdmin):
 
     list_display = ('id', 'event', 'tickets', 'ticketholder',)
     search_fields = ['event', 'ticketholder']
-
-
-# Register your models here.
