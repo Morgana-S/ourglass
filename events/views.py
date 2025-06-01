79e85fad863622baa0738a558fb769b04b8a343f
Morgana-S <st35giv9@students.codeinstitute.net>
2025-06-01 13:54:50 +0100

    Create the MyEventsDashboard and logout views

--- events/views.py
+++ events/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
+from django.contrib.auth import logout
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import redirect
+from django.utils.timezone import now
 from django.views import generic
-from .models import Event
+from .models import Event, Booking
 # Create your views here.
 
 
@@ -10,3 +14,40 @@ class LatestEventList(generic.ListView):
     """
     queryset = Event.objects.all().order_by('-created_on')[:5]
     template_name = 'events/index.html'
+
+
+class MyEventsDashboardView(LoginRequiredMixin, generic.TemplateView):
+    """
+    Returns all bookings that the user has tickets for, as well as all
+    events the user has organised in one view.
+    Also returns bookings that were previously attended to incentivize the
+    user to leave a review.
+    """
+
+    template_name = 'events/my-events.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        user = self.request.user
+        context['bookings'] = Booking.objects.filter(
+            ticketholder=user
+            ).exclude(
+            event__event_date__lt=now()
+            )
+        context['organised_events'] = Event.objects.filter(
+            event_organiser=user
+            )
+        context['previous_bookings'] = Booking.objects.filter(
+            ticketholder=user,
+            event__event_date__lt=now(),
+            ).exclude(
+                event__event_organiser=user
+            )
+
+        return context
+
+
+def logout_view(request):
+    logout(request)
+    messages.success(request, "You have been logged out.")
+    return redirect('index')
