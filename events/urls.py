cab30c899dc531201ecd9557d1fadb33b6855197
Morgana-S <st35giv9@students.codeinstitute.net>
2025-06-01 13:53:45 +0100

    Add paths for logout and my-events page

--- events/urls.py
+++ events/urls.py
@@ -3,4 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.LatestEventList.as_view(), name='index'),
+    path('logout/', views.logout_view, name='logout'),
+    path('myevents/', views.MyEventsDashboardView.as_view(), name='my-events'),
 ]
