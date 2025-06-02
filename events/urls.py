6a0ccb3f7e66dcfe82ecb633fee00655ec90c8c8
Morgana-S <st35giv9@students.codeinstitute.net>
2025-06-02 12:08:49 +0100

    Add path for individual event page

--- events/urls.py
+++ events/urls.py
@@ -2,7 +2,12 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.LatestEventList.as_view(), name='index'),
+    path(
+        'events/<int:event_id>/',
+        views.event_detail_view,
+        name='event-detail'
+        ),
     path('logout/', views.logout_view, name='logout'),
     path('myevents/', views.MyEventsDashboardView.as_view(), name='my-events'),
+    path('', views.LatestEventList.as_view(), name='index'),
 ]
