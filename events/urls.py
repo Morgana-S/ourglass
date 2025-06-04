b3627172031f8094bfbbf9399db542a1d92c9c02
Morgana-S <st35giv9@students.codeinstitute.net>
2025-06-04 18:06:52 +0100

    Add path for create-event url

--- events/urls.py
+++ events/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
         views.event_detail_view,
         name='event-detail'
         ),
+    path('events/create-event/', views.create_event_view, name='create-event'),
     path('logout/', views.logout_view, name='logout'),
     path('myevents/', views.MyEventsDashboardView.as_view(), name='my-events'),
     path('', views.LatestEventList.as_view(), name='index'),
