d9a7b838cbd658ae586f870fae85db9e93e07b05
Morgana-S <st35giv9@students.codeinstitute.net>
2025-06-04 15:55:57 +0100

    Register Message model in admin page

---
+++ contact/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from .models import Message
+from django_summernote.admin import SummernoteModelAdmin
+# Register your models here.
+
+
+@admin.register(Message)
+class MessageAdmin(SummernoteModelAdmin):
+    list_display = ('id', 'message_author', 'guest_name',
+                    'message_reason', 'created_on',)
+    search_fields = ['message_author', 'guest_name', 'message_reason']
+    list_filter = ('message_author', 'guest_name',
+                   'message_reason', 'created_on',)
