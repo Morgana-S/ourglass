534389d47f07df40b6ec68de57946e20de3dec28
Morgana-S <st35giv9@students.codeinstitute.net>
2025-06-05 11:59:58 +0100

    Create urlpath for edit-event page

--- events/urls.py
+++ events/urls.py
@@ -6,8 +6,13 @@ urlpatterns = [
         'events/<int:event_id>/',
         views.event_detail_view,
         name='event-detail'
-        ),
+    ),
     path('events/create-event/', views.create_event_view, name='create-event'),
+    path(
+        'events/edit-event/<int:event_id>/',
+        views.edit_event_view,
+        name='edit-event'
+    ),
     path('logout/', views.logout_view, name='logout'),
     path('myevents/', views.MyEventsDashboardView.as_view(), name='my-events'),
     path('', views.LatestEventList.as_view(), name='index'),
