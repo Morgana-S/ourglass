4a93382118460b2c5367f4c4e5b678003f3f2884
Morgana-S <st35giv9@students.codeinstitute.net>
2025-06-11 12:43:16 +0100

    Set up DEBUG if env.py is present

--- ourglass/settings.py
+++ ourglass/settings.py
@@ -27,7 +27,10 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+if os.path.isfile('env.py'):
+    DEBUG = True
+else:
+    DEBUG = False
 
 ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
 
