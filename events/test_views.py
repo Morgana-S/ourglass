9245b47fcbd44c8399004a6113a084e534970989
Morgana-S <st35giv9@students.codeinstitute.net>
2025-06-12 16:11:14 +0100

    Add LatestEventList View Tests

---
+++ events/test_views.py
@@ -0,0 +1,61 @@
+from django.contrib.auth.models import User
+from django.core.files.uploadedfile import SimpleUploadedFile
+from django.test import TestCase
+from django.urls import reverse
+from django.utils import timezone
+from .models import Event
+# Create your tests here.
+
+
+class LatestEventListTests(TestCase):
+    """
+    TestCase for the LatestEventList View.
+    """
+
+    def setUp(self):
+        """
+        Creates a user, and 7 different events for that user.
+        """
+        self.organiser = User.objects.create_user(
+            username='organiser',
+            email='organiser@testemail.com',
+            password='pass'
+        )
+        for i in range(7):
+            Event.objects.create(
+                event_name=f"Event {i}",
+                event_date=timezone.now() + timezone.timedelta(days=i+1),
+                created_on=timezone.now() - timezone.timedelta(days=i),
+                image='test.jpg',
+                event_organiser=self.organiser,
+                is_online=True,
+                maximum_attendees=10,
+                short_description=f"Short desc {i}",
+                long_description=f"Long desc {i}",
+            )
+
+    def test_view_status_code_and_template(self):
+        """
+        Tests whether the request is successful and whether the right template
+        is used.
+        """
+        response = self.client.get(reverse('index'))
+        self.assertEqual(response.status_code, 200,)
+        self.assertTemplateUsed(response, 'events/index.html')
+
+    def test_view_context_contains_latest_5_events(self):
+        """
+        Tests whether the event list contains five events.
+        """
+        response = self.client.get(reverse('index'))
+        events = response.context['event_list']
+        self.assertEqual(len(events), 5)
+
+    def test_events_ordered_by_created_on(self):
+        """
+        Tests the events in the event_list are ordered by created_on date.
+        """
+        response = self.client.get(reverse('index'))
+        events = list(response.context['event_list'])
+        for i in range(len(events) - 1):
+            self.assertTrue(events[i].created_on >= events[i + 1].created_on)
