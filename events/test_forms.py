9a8b0cf99bdfd877ffd43d038a0561cfe9f5e177
Morgana-S <st35giv9@students.codeinstitute.net>
2025-06-12 14:02:25 +0100

    Write tests for EventForm

---
+++ events/test_forms.py
@@ -0,0 +1,130 @@
+from django.test import TestCase
+from django.utils import timezone
+from datetime import timedelta
+from django.core.files.uploadedfile import SimpleUploadedFile
+from .forms import EventForm
+# Create your tests here.
+
+
+class TestEventForm(TestCase):
+    """
+    Test Case for all Event Form tests.
+    """
+
+    def setUp(self):
+        """
+        Creates a reusable setup for testing form data.
+        """
+        self.image = SimpleUploadedFile(
+            name='test_image.jpg',
+            content=b'test',
+            content_type='image/jpeg'
+        )
+        self.form_data = {
+            'event_name': 'test event',
+            'event_date': (timezone.now() + timedelta(days=2)).strftime(
+                '%Y-%m-%d %H:%M:%S'
+            ),
+            'is_online': False,
+            'url_or_address': '123 Tes Street',
+            'maximum_attendees': 100,
+            'short_description': 'Test Event',
+            'long_description': 'Test Event Long Description'
+        }
+
+    def test_form_is_valid(self):
+        """
+        Tests if the form data is valid.
+        """
+        self.valid_form = EventForm(
+            data=self.form_data,
+            files={'image': self.image}
+        )
+        self.assertTrue(self.valid_form.is_valid(), msg='Form is invalid')
+
+    def test_event_date_in_past(self):
+        """
+        Tests if an event date is in the past. Also confirms that there 
+        are errors in the form if the data is incorrect.
+        """
+        data = self.form_data.copy()
+        data['event_date'] = (timezone.now() - timedelta(days=2)).strftime(
+            '%Y-%m-%d %H:%M:%S'
+        )
+        form = EventForm(data=data, files={'image': self.image})
+        self.assertFalse(form.is_valid(), msg='event date is valid')
+        self.assertIn('event_date', form.errors)
+
+    def test_event_date_too_far_in_future(self):
+        """
+        Tests if an event date is too far in the future.
+        Also confirms that there are errors in the form if the data is
+        incorrect.
+        """
+        data = self.form_data.copy()
+        data['event_date'] = (timezone.now() + timedelta(days=1000)).strftime(
+            '%Y-%m-%d %H:%M:%S'
+        )
+        form = EventForm(data=data, files={'image': self.image})
+        self.assertFalse(form.is_valid(), msg='event date is valid')
+        self.assertIn('event_date', form.errors)
+
+    def test_maximum_attendees_above_limit(self):
+        """
+        Tests if the event attendees exceed the limit of 200.
+        Also confirms that there are errors in the form if the data is
+        incorrect.
+        """
+        data = self.form_data.copy()
+        data['maximum_attendees'] = 300
+        form = EventForm(data=data, files={'image': self.image})
+        self.assertFalse(form.is_valid(), msg='number of attendees is valid')
+        self.assertIn('maximum_attendees', form.errors)
+
+    def test_maximum_attendees_below_limit(self):
+        """
+        Tests if the event attendees is below the limit of 1.
+        Also confirms that there are errors in the form if the data is
+        incorrect.
+        """
+        data = self.form_data.copy()
+        data['maximum_attendees'] = 0
+        form = EventForm(data=data, files={'image': self.image})
+        self.assertFalse(form.is_valid(), msg='number of attendees is valid')
+        self.assertIn('maximum_attendees', form.errors)
+
+    def test_missing_required_fields(self):
+        """
+        Tests if the form is valid when required fields are missing.
+        Also confirms that there are errors in the form if the data is
+        incorrect.
+        """
+        form = EventForm(data={}, files={})
+        self.assertFalse(
+            form.is_valid(),
+            msg='form is valid without req fields'
+        )
+        required_fields = [
+            'event_name', 'event_date', 'image', 'url_or_address',
+            'maximum_attendees', 'short_description', 'long_description'
+        ]
+        for field in required_fields:
+            self.assertIn(field, form.errors)
+
+    def test_is_online_checkbox_true(self):
+        """
+        Tests if the checkbox for is_online is marked as true.
+        """
+        data = self.form_data.copy()
+        data['is_online'] = True
+        form = EventForm(data=data, files={'image': self.image})
+        self.assertTrue(form.is_valid())
+
+    def test_is_online_checkbox_false(self):
+        """
+        Tests if the checkbox for is_online is marked as false.
+        """
+        data = self.form_data.copy()
+        data['is_online'] = False
+        form = EventForm(data=data, files={'image': self.image})
+        self.assertTrue(form.is_valid())
