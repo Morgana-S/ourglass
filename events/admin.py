296606ff7a4fbf956f26e1064db676792a2a2c7b
Morgana-S <st35giv9@students.codeinstitute.net>
2025-06-13 16:56:38 +0100

    Update admin fields with working search functionality

--- events/admin.py
+++ events/admin.py
@@ -7,21 +7,24 @@ from django_summernote.admin import SummernoteModelAdmin
 @admin.register(Event)
 class EventAdmin(SummernoteModelAdmin):
 
-    list_display = ('id', 'event_name', 'event_date', 'event_organiser', 'created_on', 'updated_on',)
+    list_display = ('id', 'event_name', 'event_date', 'event_organiser',
+                    'created_on', 'updated_on',)
     search_fields = ['event_name']
     summernote_fields = ('long_description',)
-    list_filter = ('event_organiser', 'event_date', 'created_on', 'updated_on',)
+    list_filter = ('event_organiser', 'event_date', 'created_on', 'updated_on')
 
 
 @admin.register(Booking)
 class BookingAdmin(SummernoteModelAdmin):
 
     list_display = ('id', 'event', 'tickets', 'ticketholder',)
-    search_fields = ['event', 'ticketholder']
+    search_fields = ['event__event_name', 'ticketholder__username']
+    list_filter = ('ticketholder', 'event',)
 
 
 @admin.register(Review)
 class ReviewAdmin(SummernoteModelAdmin):
 
     list_display = ('id', 'event', 'author', 'rating',)
-    search_fields = ['event', 'author']
+    search_fields = ['author', 'event__event_name']
+    list_filter = ('rating', 'author')
